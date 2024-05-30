4e0c4a67023608ab09febd3e0ddcb6a12872be93
andybramer88 <andybramer@gmx.at>
2024-05-30 19:50:13 +0200

    full

--- app/src/main/java/com/example/mybooks/MainActivity.kt
+++ app/src/main/java/com/example/mybooks/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mybooks
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
@@ -69,5 +70,35 @@ class MainActivity : ComponentActivity() {
         setContent {
             Navigation()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart called.")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume called.")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("MainActivity", "onPause called.")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("MainActivity", "onStop called.")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("MainActivity", "onRestart called.")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("MainActivity", "onDestroy called.")
     }
 }
